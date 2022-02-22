5bc6f6469de18bd7ab35dd6eadf8929598305099
godalming123 <jamiesmglass@gmail.com>
2022-02-22 11:47:18 +0000

    improve updates notifucations

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,24 @@
+# ===============
+# === UPDATES ===
+# ===============
+function notify_updates
+    paru -S # download databases
+    paru -Qu >~/.config/updates.txt # push updates into textfile
+    set updates (cat ~/.config/updates.txt | wc -l) # set a variable for the updates
+    if test $updates -ne 0
+        notify-send --urgency=low --icon="" "You have $updates updates" # notify user of the updates
+    end
+end
+
 # ==================
 # === HANDLE TTY ===
 # ==================
 
 set ttyVar (tty)
 
-if test $ttyVar = "/dev/tty1" # if we are running in tty
-    dbus-run-session wayfire # run wayfire
+if test $ttyVar = /dev/tty1 # if we are running in tty
+    dbus-run-session wayfire & # run wayfire
+    watch -n (expr 60 \* 60 \* 24) notify_updates # handle notifying updates every day
 end
 
 # =====================
@@ -31,26 +44,18 @@ set white (tput setaf 7) &
 # ====================
 
 if status is-interactive
-    # ===============
-    # === UPDATES ===
-    # ===============
-
-    paru -Qu > ~/.config/updates.txt &
-
-    # ===================
     # === Add aliases ===
-    # ===================
 
-    # === ls aliases ===
+    # = ls aliases =
     alias ls='ls --color=auto' &
     alias ll='ls -lav --ignore=..' & # show a long listing of all files/folders except ".."
-    alias l='ls -lav -ignore=.?*' &  # show a long listing but no hidden files except "."
+    alias l='ls -lav -ignore=.?*' & # show a long listing but no hidden files except "."
 
-    # === app aliases ===
+    # = app aliases =
     alias teams='flatpak run com.microsoft.Teams &'
     alias vscodium='flatpak run com.vscodium.codium &'
 
-    # === git aliases ===
+    # = git aliases =
     alias gstat='git status' &
     alias gadd='git add' &
     alias gclone='git clone' &
@@ -60,8 +65,8 @@ if status is-interactive
     alias gomote='git remote' &
     alias giff='git diff' &
     alias gog='git log' &
-    
-    # === other aliases ===
+
+    # = other aliases =
     alias screens='kanshi' &
     alias config='cd ~/Documents/coding\ repos/dotfiles/; micro fish/config.fish sys-info/ufetch-endevour.sh wayfire/wayfire.ini wayfire/wf-shell.ini alacritty/alacritty.yml wofi/styles.css wofi/wifi wofi/wofi-wifi.sh mako/config' &
     alias pacman='sudo pacman' &
@@ -87,7 +92,7 @@ end
 # === updates ===
 function updates
     set updates (cat ~/.config/updates.txt | wc -l)
-    if test "$updates" != "0"; or test "$updates" != ""
+    if test "$updates" != 0; or test "$updates" != ""
         echo -n "$reset with $red$updates$reset updates"
     end
 end
@@ -95,30 +100,26 @@ end
 # === git ===
 function git_info
     set branch_name (git rev-parse --abbrev-ref HEAD 2> /dev/null)
-    
+
     if test "$branch_name" != ""
         echo -n "$reset on$cyan $branch_name" # if we are on a git folder
         git_changes
     end
 end
 
-function git_branch
-    
-end
-
 function git_changes
     # /dev/null prevents nonsensical errors when you are on directories not tracked by git
     set changesLNs (git status --untracked-files=no 2> /dev/null | wc -l)
     set changes (expr $changesLNs - 9)
-    
+
     if test $changes -eq 0
         set changesText "no changes"
     else if test $changes -gt 1
         set changesText "$changes changes"
     else
-        set changesText "1 change" 
+        set changesText "1 change"
     end
-    
+
     echo -n "$reset with$cyan $changesText"
 end
 
@@ -142,7 +143,7 @@ end
 
 # === prompt indicator ===
 function indicator
-    if test "$argv" = "true"
+    if test "$argv" = true
         echo -n "$green > "
     else
         echo -n "$red > "
@@ -161,5 +162,4 @@ function fish_prompt
     git_info
     updates
     indicator "$success"
-        
 end
