d34790ce2531da73d81f04382a1fa8acbee340b4
godalming123 <jamiesmglass@gmail.com>
2022-03-12 13:28:06 +0000

    small improvements

--- eww/scripts/search.sh
+++ eww/scripts/search.sh
@@ -5,6 +5,6 @@ buf=""
 for name in $list ; do
   buf="$buf \n (button :vexpand true :halign \"fill\" :valign \"fill\" :class \"btn\" :style \"padding: 4px 2px;font-size: 15px;margin: 0;\" :onclick \"$name\" \"$name\" )"
 done
-result="(box :orientation \"v\" :class \"apps\" :halign \"fill\" :valign \"fill\" :vexpand true $buf\n)"
+result="(box :orientation \"v\" :class \"apps\" :halign \"fill\" :valign \"fill\" :vexpand true :space-evenly false $buf\n)"
 echo -e "$result" > search-items.txt
 echo -e "$result"
