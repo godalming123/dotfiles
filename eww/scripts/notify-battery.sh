86da1d411d33ede5daca3726617d48c3e97109df
godalming123 <jamiesmglass@gmail.com>
2022-03-13 09:54:45 +0000

    add bluetooth menu

--- eww/scripts/notify-battery.sh
+++ eww/scripts/notify-battery.sh
@@ -3,5 +3,5 @@
 charge=$(cat /sys/class/power_supply/BAT0/capacity)
 
 if [ "$charge" -lt 20 ]; then
-    notify-send -t 5000 "Running low on juice" "You have $charge% charge left"
+    notify-send -t 5000 -i "~/.config/eww/icons/dark/battery.png" "Running low on juice" "You have $charge% charge left"
 fi
