2d5660ad100b23e0a382e3a76c6c37c1a9e75a26
godalming123 <jamiesmglass@gmail.com>
2022-03-13 15:53:13 +0000

    gerneral improvements

--- eww/scripts/search.sh
+++ eww/scripts/search.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 list=$(ls /usr/bin/ | grep -m 20 -i "$1")
 
-buf=""
-for name in $list ; do
-  buf="(button :hexpand true :halign \"fill\" :valign \"fill\" :class \"btn\" :style \"padding: 4px 2px;font-size: 15px;margin: 0;\" :onclick \"eww close menu & $name &\" \"$name\" ) $buf"
-done
-result="(box :orientation \"v\" :class \"apps\" :halign \"fill\" :valign \"fill\" :hexpand true :space-evenly false $buf)"
+if ["$1" == ""]; then
+  result="(label :text \"test\")"
+else
+  buf=""
+  for name in $list ; do
+    buf="(button :hexpand true :halign \"fill\" :valign \"fill\" :class \"btn\" :style \"padding: 4px 2px;font-size: 15px;margin: 0;\" :onclick \"eww close menu & $name &\" \"$name\" ) $buf"
+  done
+  result="(box :orientation \"v\" :class \"apps\" :halign \"fill\" :valign \"fill\" :hexpand true :space-evenly false $buf)"
+fi
 
 eww update search_listen="$result"
--- eww/scripts/wifi.sh
+++ eww/scripts/wifi.sh
@@ -1,31 +1,71 @@
-#!/bin/bash
+#!/bin/sh
 
-status=$(nmcli g | grep -oE "disconnected")
-essid=$(nmcli c | grep wlan0 | awk '{print ($1)}')
+connected=$(nmcli c | grep wlan0 | awk '{print ($1)}')
+help_text="WIFI-MAN - a tool to connect to wifi networks
+=============================================
+--ESSID           print the currently connected network (blank string when unconnected)
+--LIST            list avialable wifi networks
+--SIMPLE-LIST     list only name of avialable wifi networks
+--COMPLEX-LIST       list all properties of wifi networks
+--LIST-FORMAT     list all properties of wifi networks with an eww format
+--TOGGLE          toggle wifi on/off
+--CONNECT-UNKNOWN connect to an unknown network where 2nd argument is name and 3rd argument is password
+--CONNECT-KNOWN   connect to a known network where 2nd argument is the network name
+--CONNECT         an aliases to connect-unknown TODO: dynamically change between connect unknown and connect-known depending on if the network is known
+--DISCONNECT      disconnect from connected network
+--INFO            show basic info on device hardware
+=============================================
+if argument mathces none of the above this help text will be show
+"
 
 if [[ "$1" == "--ESSID" ]]; then
-	echo $essid
-elif [[ "$1" == "--UPDATE-MENU" ]]; then
+	echo "$connected"
 
-# === DEFINE VARS ===
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-FIELDS=SSID,SECURITY
-LIST=$(nmcli --fields "$FIELDS" device wifi list | sed '/^--/d') # avialiable networks
-KNOWNCON=$(nmcli connection show) # Gives a list of known connections so we can parse it later
-CONSTATE=$(nmcli -fields WIFI g) # Really janky way of telling if there is currently a connection
-CURRSSID=$(LANGUAGE=C nmcli -t -f active,ssid dev wifi | awk -F: '$1 ~ /^yes/ {print $2}')
+elif [[ "$1" == "--LIST" ]]; then
+	echo "$(nmcli --fields "SSID,SECURITY,IN-USE" dev wifi list)"
 
-if [[ ! -z $CURRSSID ]]; then
-    HIGHLINE=$(echo  "$(echo "$LIST" | awk -F "[  ]{2,}" '{print $1}' | grep -Fxn -m 1 "$CURRSSID" | awk -F ":" '{print $1}') + 1" )
-fi
+elif [[ "$1" == "--SIMPLE-LIST" ]]; then
+	echo "$(nmcli --fields "SSID" dev wifi list)"
 
-if [[ "$CONSTATE" =~ "enabled" ]]; then
-    TOGGLE="toggle wifi off"
-elif [[ "$CONSTATE" =~ "disabled" ]]; then
-    TOGGLE="toggle wifi on"
-fi
+elif [[ "$1" == "--COMPLEX-LIST" ]]; then
+	echo "$(nmcli dev wifi list)"
 
-TEXT=$(echo -e "$TOGGLE\nmanual\n$LIST" | uniq -u)
+elif [[ "$1" == "--LIST-FORMAT" ]]; then
+    IFS=$'\n'       # make newlines the only separator
+    set -f          # disable globbing
 
-echo -e "$TEXT"
+	list=$(./wifi.sh --LIST | tail -n +2)
+	headers=$(./wifi.sh --LIST | head -n 1)
+
+    buf=""
+    for line in $list; do
+        network_name=$(echo "$line" | awk '{print $1}')
+        buf="(button :hexpand true :halign \"fill\" :class \"btn\" :style \"padding: 4px 2px;font-size: 15px;margin: 0;\" :onclick \"./wifi.sh --CONNECT $network_name\" \"$line\" )\n    $buf"
+    done
+    echo -e "(box :orientation \"v\" :class \"wifi-networks\" :halign \"fill\" :valign \"end\" :hexpand true :space-evenly false\n    (label :text \"$headers\")\n    $buf)"
+
+elif [[ "$1" == "--TOGGLE" ]]; then
+    if [[ "$connected" == "" ]]; then
+        nmcli radio wifi on
+    else
+        nmcli radio wifi off
+    fi
+
+elif [[ "$1" == "--CONNECT-UNKNOWN" ]]; then
+    sudo nmcli dev wifi connect $2 password $3
+
+elif [[ "$1" == "--CONNECT-KNOWN" ]]; then
+    nmcli con up $2
+
+elif [[ "$1" == "--CONNECT" ]]; then
+    ./wifi.sh --CONNECT-UNKNOWN $2 $3
+
+
+elif [[ "$1" == "--DISCONNECT" ]]; then
+    nmcli con down $connected
+
+elif [[ "$1" == "--INFO" ]]; then
+    echo "$(nmcli -o)"
+else
+    echo -e "$help_text"
 fi
