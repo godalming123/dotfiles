17e35fd1dc88cda86ee461f0482248883677c791
godalming123 <r2hk9ahnf@relay.firefox.com>
2022-05-30 18:26:03 +0100

    improve

--- 1-install/git-config.sh
+++ 1-install/git-config.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/sh
 git config --global credential.helper store # make sure that git stores our credentials and remembers them
+git config --global init.defaultBranch master # set default branch when initing a folder so git doesnt through warnings
